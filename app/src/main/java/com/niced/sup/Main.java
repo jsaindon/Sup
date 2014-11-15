5a31436c058aee9c0f3bcef507d2e244e3a7c8aa
James Saindon <jamessaindon@gmail.com>
2014-11-15 03:16:06 -0800

    Fixed layout + added ability to add contacts to message recipient list.

--- app/src/main/java/com/niced/sup/Main.java
+++ app/src/main/java/com/niced/sup/Main.java
@@ -2,19 +2,42 @@ package com.niced.sup;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class Main extends Activity {
+    String DEBUG_TAG = "Sup";
+
+    // Info of Contacts we're sending "Sup" to
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayList<String> phones = new ArrayList<String>();
+
+    // Array Adapter to update contents of ListView containing contact names
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find list view
+        ListView listView = (ListView) findViewById(R.id.contacts_listview);
+
+        // Set adapter to list view
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
     }
 
 
@@ -38,24 +61,43 @@ public class Main extends Activity {
     }
 
     private static final int CONTACT_PICKER_RESULT = 1001;
-//
-//    public void doLaunchContactPicker(View view) {
-//        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, Contents.CONTENT_URI);
-//        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
-//    }
-//
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (resultCode == RESULT_OK) {
-//            switch (requestCode) {
-//                case CONTACT_PICKER_RESULT:
-//                    Uri result = data.getData();
-//                    String id = result.getLastPathSegment();
-//                    break;
-//            }
-//
-//        } else {
-//            // gracefully handle failure
-//            Log.w(DEBUG_TAG, "Warning: activity result not ok");
-//        }
-//    }
+
+    public void doLaunchContactPicker(View view) {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    // Get contact's name and number
+                    Uri contactData = data.getData();
+                    String[] contacts = new String[] {Contacts.DISPLAY_NAME, Phone.NUMBER};
+                    Cursor cursor = getContentResolver().query(contactData, contacts, null, null, null);
+                    cursor.moveToFirst();
+
+                    // get phone number if contact has one
+                    int phoneIndex = cursor.getColumnIndex(Phone.NUMBER);
+                    int nameIndex = cursor.getColumnIndex(Contacts.DISPLAY_NAME);
+
+                    String name = cursor.getString(nameIndex);
+                    String phone = cursor.getString(phoneIndex);
+
+                    cursor.close();
+
+                    // Add contact info to lists
+                    phones.add(phone);
+
+                    // Notify adapter
+                    adapter.add(name);
+
+                    break;
+            }
+
+        } else {
+            // gracefully handle failure
+            Log.w(DEBUG_TAG, "Warning: activity result not ok");
+        }
+    }
 }
