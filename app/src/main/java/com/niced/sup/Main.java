a6d74220f9cb5556de3da1bbb5014ef1a525ee3b
Anne Christy <achristy@hmc.edu>
2014-11-15 02:22:44 -0800

    working sms tutorial

--- app/src/main/java/com/niced/sup/Main.java
+++ app/src/main/java/com/niced/sup/Main.java
@@ -1,4 +1,4 @@
-package com.niced.sup;
+/*package com.niced.sup;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -32,5 +32,69 @@ public class Main extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+}
+*/
+
+package com.niced.sup;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sendsmsdemo.R;
+
+
+public class Main extends Activity {
+
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        txtphoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+        txtMessage = (EditText) findViewById(R.id.editTextSMS);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+
+    }
+    protected void sendSMSMessage() {
+        Log.i("Send SMS", "");
+
+        String phoneNo = txtphoneNo.getText().toString();
+        String message = txtMessage.getText().toString();
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again.",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
